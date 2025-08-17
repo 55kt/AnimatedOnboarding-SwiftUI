600144fc55fa9bca00494518b0b93e7987c39f09
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-17 09:28:14 +0300

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Vlad on 17/8/25.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
